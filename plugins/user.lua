401ec259256b9857cd991c53ec3b27df717116d6
Adriano Viana <adrianoviana87@gmail.com>
2023-04-06 17:49:06 -0300

    use official copilot

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,44 +1,16 @@
 return {
-   -- You can also add new plugins here as well:
-   -- Add plugins, the lazy syntax
-   -- "andweeb/presence.nvim",
-   -- {
-   --   "ray-x/lsp_signature.nvim",
-   --   event = "BufRead",
-   --   config = function()
-   --     require("lsp_signature").setup()
-   --   end,
-   -- },
-   {
-      "zbirenbaum/copilot.lua",
-      enabled = true,
-      cmd = "Copilot",
-      event = "InsertEnter",
-      opts = {
-         suggestion = { enabled = true },
-         panel = { enabled = true },
-      },
-   },
-
-   {
-      "simrat39/symbols-outline.nvim",
-      keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-      config = true,
-   },
-
-   {
-      "nvim-cmp",
-      dependencies = {
-         "hrsh7th/cmp-emoji",
-         {
-            "zbirenbaum/copilot-cmp",
-            opts = {},
-         },
-      },
-      ---@param opts cmp.ConfigSchema
-      opts = function(_, opts)
-         local cmp = require("cmp")
-         opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" }, { name = "emoji" } }))
-      end,
-   },
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "github/copilot.vim",
+    lazy = false
+  }
 }
