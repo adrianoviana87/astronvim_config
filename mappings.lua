a7bbd41d9c9d5da4a46180447e1fc1b26fd97a81
Adriano Viana <adrianoviana87@gmail.com>
2023-04-06 17:39:16 -0300

    add copilot

--- mappings.lua
+++ mappings.lua
@@ -9,6 +9,7 @@ return {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     [";"] = { ":" },
+    ["<leader>pf"] = { "<cmd>echo expand('%')<cr>", desc = "Print file path" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
