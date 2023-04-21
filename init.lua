60189ae483cca8d81735ab010549eda9f00ee1bf
Adriano Viana <adrianoviana87@gmail.com>
2023-04-21 20:16:57 -0300

    fix colorscheme and scrolloff

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin-latte",
+  colorscheme = "astrotheme",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
